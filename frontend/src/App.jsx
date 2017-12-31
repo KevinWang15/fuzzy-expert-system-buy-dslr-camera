f7b9d0d1fc95605ea25dda5fd9ab0c8af8957bf8
Ke Wang <453587854@qq.com>
2017-12-31 12:14:36 +0800

    use antd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,39 @@
 import React from 'react';
-import "./bulmaswatch.min.css";
 import "./index.css";
-import { Button, Hero, Container, Title, SubTitle, Checkbox } from 'reactbulma';
+import { Layout, Menu, Breadcrumb } from 'antd';
+const { Header, Content, Footer } = Layout;
 
 let questions = [
-  {text:"您将如何使用本相机（多选）",multiple:true,options:['记录旅行']}
+  { text: "您将如何使用本相机（多选）", multiple: true, options: ['记录旅行'] },
 ];
 
 export default class App extends React.Component {
   render() {
-    return <Hero medium dark bold fullheight>
-      <Hero.Body>
-        <Container>
-          <Title>
-            相机推荐专家系统
-          </Title>
-          <SubTitle style={{ opacity: 0.5 }}>
-            人工智能Project by 王轲(14307130048)
-          </SubTitle>
+    return <Layout>
+      <Header style={{ position: 'fixed', width: '100%' }}>
+        <div className="logo"/>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          style={{ lineHeight: '64px' }}
+        >
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px', marginTop: 64 }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item> 相机推荐专家系统
+          </Breadcrumb.Item>
+        </Breadcrumb>
+        <div style={{ background: '#fff', padding: 24, minHeight: 380 }}>获得推荐</div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        人工智能Project by 王轲(14307130048)
 
-          <div>
-            <Checkbox>aaa</Checkbox>
-          </div>
-
-          <Button primary>获得推荐</Button>
-
-        </Container>
-      </Hero.Body>
-    </Hero>;
+      </Footer>
+    </Layout>;
   }
 }
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -5,13 +5,26 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.less$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "less-loader" // compiles Less to CSS
+        }],
+      },
+      {
         test: /\.jsx?/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: ["es2015", "react"],
-            plugins: ["transform-object-rest-spread"],
+            plugins: ["transform-object-rest-spread", ["import", {
+              "libraryName": "antd",
+              "style": true,   // or 'css'
+            }]],
           },
         },
       },
